775a0f482e91f3225c018f5f27c164e207dddc34
MaximoAndriola <maximoandriola2017@gmail.com>
2025-08-15 16:32:13 -0300

    feat(address)
    entity address created

---
+++ src/main/java/PSG/backEnd/model/entity/Address.java
@@ -0,0 +1,32 @@
+package PSG.backEnd.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Address {
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private int number;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String zipCode;
+}
