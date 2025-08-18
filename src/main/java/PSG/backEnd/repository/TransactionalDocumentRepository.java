115ef22cb6c2ce09ed24d76f9e3e7c56b25bf23f
MaximoAndriola <maximoandriola2017@gmail.com>
2025-08-18 13:03:30 -0300

    feat(transactionalDocumentRespository)
    Introduces a JPA repository for TransactionalDocument entities with custom query methods for filtering by document number, supplier, amount, date, and deletion status.

---
+++ src/main/java/PSG/backEnd/repository/TransactionalDocumentRepository.java
@@ -0,0 +1,48 @@
+package PSG.backEnd.repository;
+
+import PSG.backEnd.model.entity.Supplier;
+import PSG.backEnd.model.entity.TransactionalDocument;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface TransactionalDocumentRepository extends JpaRepository<TransactionalDocument, Long> {
+    List<TransactionalDocument> findByDeletedFalse();
+    Optional<TransactionalDocument> findByIdAndDeletedFalse(Long id);
+    Optional<TransactionalDocument> findByBranchCodeAndDocumentNumberAndDeletedTrue(String branchCode, String documentNumber);
+    boolean existsByBranchCodeAndDocumentNumberAndDeletedFalse(String branchCode, String documentNumber);
+
+    @Query("""
+            SELECT td FROM TransactionalDocument td
+            WHERE (:documentNumber IS NULL OR td.documentNumber LIKE %:documentNumber%)
+            AND (:supplierCuit IS NULL OR td.supplier.cuit LIKE %:supplierCuit%)
+            AND (:supplierName IS NULL OR 
+                 LOWER(td.supplier.legalName) LIKE LOWER(CONCAT('%', :supplierName, '%')) OR
+                 LOWER(td.supplier.tradeName) LIKE LOWER(CONCAT('%', :supplierName, '%'))
+            )
+            AND (:maxTotalAmount IS NULL OR td.total <= :maxTotalAmount)
+            AND (:minTotalAmount IS NULL OR td.total >= :minTotalAmount)
+            AND (:totalAmount IS NULL OR td.total = :totalAmount)
+            AND (:fromDate IS NULL OR td.date >= :fromDate)
+            AND (:toDate IS NULL OR td.date <= :toDate)
+            AND td.deleted = false
+           """)
+    Page<TransactionalDocument> findAllWithFilters(
+            @Param("documentNumber") String documentNumber,
+            @Param("supplierCuit") String supplierCuit,
+            @Param("supplierName") String supplierName,
+            @Param("maxTotalAmount") BigDecimal maxTotalAmount,
+            @Param("minTotalAmount") BigDecimal minTotalAmount,
+            @Param("totalAmount") BigDecimal totalAmount,
+            @Param("fromDate") LocalDate fromDate,
+            @Param("toDate") LocalDate toDate,
+            Pageable pageable
+    );
+}
