2c730a264495259c12bb0af5a812c3458bde525b
MaximoAndriola <maximoandriola2017@gmail.com>
2025-09-21 00:27:50 -0300

    feat(vehicle entity)

---
+++ src/main/java/PSG/backEnd/model/entity/Vehicle.java
@@ -0,0 +1,63 @@
+package PSG.backEnd.model.entity;
+
+import PSG.backEnd.model.entity.GasStation.FuelLoad;
+import PSG.backEnd.model.enums.VehicleType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "vehicles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, name = "license_plate")
+    private String licensePlate;
+
+    @Column
+    private String brand;
+
+    @Column
+    private String model;
+
+    @Column
+    private Integer year;
+
+    @Column
+    private String color;
+
+    @Column(name = "nick_name")
+    private String nickName;
+
+    @Column(name = "vehicle_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private VehicleType vehicleType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_area_id")
+    private ProjectArea projectArea;
+
+    @Column(name = "stored_in")
+    private String storedIn;
+
+    @Column(name = "vtv_expiration_date")
+    private LocalDate vtvExpirationDate;
+
+    @Column
+    private boolean deleted;
+
+    @OneToMany(mappedBy = "vehicle", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+    @Builder.Default
+    private List<FuelLoad> fuelLoads = new ArrayList<>();
+}
