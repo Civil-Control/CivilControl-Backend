72289205cfe0dfa9b11dabe72a10642a3a80f20e
MaximoAndriola <maximoandriola2017@gmail.com>
2025-10-07 12:48:49 -0300

    feat(RepairService)
    Introduces the RepairService implementation and its corresponding IRepairService interface for managing vehicle repairs. Includes methods for creating, retrieving, updating, deleting, and filtering repairs, with validation for vehicle and supplier existence.

---
+++ src/main/java/PSG/backEnd/service/port/IRepairService.java
@@ -0,0 +1,16 @@
+package PSG.backEnd.service.port;
+
+import PSG.backEnd.model.dto.vehicle.RepairDTO;
+import PSG.backEnd.model.dto.vehicle.RepairFilterDTO;
+import PSG.backEnd.model.dto.vehicle.RepairResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IRepairService {
+    RepairResponseDTO createRepair(RepairDTO repairDTO);
+    RepairResponseDTO getRepairById(Long id);
+    RepairResponseDTO updateRepair(Long id, RepairDTO repairDTO);
+    void deleteRepair(Long id);
+    Page<RepairResponseDTO> getAllRepairs(RepairFilterDTO filterDTO, Pageable pageable);
+}
+
