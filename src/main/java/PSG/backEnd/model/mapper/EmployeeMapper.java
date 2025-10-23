19cb9a0a1a2930a703fd0dc7ebdb4b2c857b1f0b
MaximoAndriola <maximoandriola2017@gmail.com>
2025-10-23 20:46:45 -0300

    feat(employee mapper)
    Introduces EmployeeMapper using MapStruct to handle conversions between EmployeeDTO, EmployeeResponseDTO, and Employee entities. Includes methods for entity creation, response mapping, and partial updates, supporting nested mappers for related objects.

---
+++ src/main/java/PSG/backEnd/model/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package PSG.backEnd.model.mapper;
+
+import PSG.backEnd.model.dto.employee.EmployeeDTO;
+import PSG.backEnd.model.dto.employee.EmployeeResponseDTO;
+import PSG.backEnd.model.entity.employee.Employee;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring", uses = {AddressMapper.class, EmergencyContactMapper.class, ProjectAreaMapper.class})
+public interface EmployeeMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "deleted", constant = "false")
+    @Mapping(target = "projectArea.id", source = "projectAreaId")
+    Employee toEntity(EmployeeDTO employeeDTO);
+
+    @Mapping(source = "projectArea", target = "projectArea")
+    EmployeeResponseDTO toResponseDto(Employee employee);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "deleted", ignore = true)
+    @Mapping(target = "projectArea.id", source = "projectAreaId")
+    void partialUpdate(EmployeeDTO updateDTO, @MappingTarget Employee employee);
+}
+
