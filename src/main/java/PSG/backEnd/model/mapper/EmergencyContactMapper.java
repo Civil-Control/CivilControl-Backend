995858128518580025961186d0551b81e6de7e9a
MaximoAndriola <maximoandriola2017@gmail.com>
2025-10-23 20:48:18 -0300

    feat(emergencyContact mapper)
    Introduces EmergencyContactMapper using MapStruct for mapping between EmergencyContact DTOs and entity. Includes methods for entity conversion, response DTO mapping, and partial updates.

---
+++ src/main/java/PSG/backEnd/model/mapper/EmergencyContactMapper.java
@@ -0,0 +1,18 @@
+package PSG.backEnd.model.mapper;
+
+import PSG.backEnd.model.dto.employee.EmergencyContactDTO;
+import PSG.backEnd.model.dto.employee.EmergencyContactResponseDTO;
+import PSG.backEnd.model.entity.employee.EmergencyContact;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface EmergencyContactMapper {
+
+    EmergencyContact toEntity(EmergencyContactDTO emergencyContactDTO);
+
+    EmergencyContactResponseDTO toResponseDto(EmergencyContact emergencyContact);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void partialUpdate(EmergencyContactDTO updateDTO, @MappingTarget EmergencyContact emergencyContact);
+}
+
