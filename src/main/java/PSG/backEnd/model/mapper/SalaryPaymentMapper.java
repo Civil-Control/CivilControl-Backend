a3b5b69f3bea473cdd033988e76b564f7e513d4f
MaximoAndriola <maximoandriola2017@gmail.com>
2025-10-23 20:52:37 -0300

    feat(SalaryPayment mapper)
    Introduces SalaryPaymentMapper using MapStruct for mapping between SalaryPayment entities and DTOs. Includes methods for entity conversion, response DTO mapping, and partial updates.

---
+++ src/main/java/PSG/backEnd/model/mapper/SalaryPaymentMapper.java
@@ -0,0 +1,25 @@
+package PSG.backEnd.model.mapper;
+
+import PSG.backEnd.model.dto.employee.SalaryPaymentDTO;
+import PSG.backEnd.model.dto.employee.SalaryPaymentResponseDTO;
+import PSG.backEnd.model.entity.employee.SalaryPayment;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface SalaryPaymentMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "employee.id", source = "employeeId")
+    SalaryPayment toEntity(SalaryPaymentDTO salaryPaymentDTO);
+
+    @Mapping(source = "employee.id", target = "employeeId")
+    @Mapping(source = "employee.name", target = "employeeName")
+    @Mapping(source = "employee.lastName", target = "employeeLastName")
+    SalaryPaymentResponseDTO toResponseDto(SalaryPayment salaryPayment);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "employee.id", source = "employeeId")
+    void partialUpdate(SalaryPaymentDTO updateDTO, @MappingTarget SalaryPayment salaryPayment);
+}
+
