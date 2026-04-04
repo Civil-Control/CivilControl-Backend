1f168ab5fb7e7e362c8b24ec7030c3d3f766a981
MaximoAndriola <maximoandriola2017@gmail.com>
2026-04-04 16:33:08 -0300

    fix: CrewAssignmentNotFoundException extends NotFoundException for proper 404 handling

--- src/main/java/PSG/backEnd/exception/crewAssignment/CrewAssignmentNotFoundException.java
+++ src/main/java/PSG/backEnd/exception/crewAssignment/CrewAssignmentNotFoundException.java
@@ -1,8 +1,9 @@
 package PSG.backEnd.exception.crewAssignment;
 
+import PSG.backEnd.exception.NotFoundException;
 import PSG.backEnd.service.util.MessageSourceHelper;
 
-public class CrewAssignmentNotFoundException extends RuntimeException {
+public class CrewAssignmentNotFoundException extends NotFoundException {
     public CrewAssignmentNotFoundException(Long id) {
         super(MessageSourceHelper.getMessageStatic("crewAssignment.notFound", id));
     }
