7968956af0f0179bfb8275038df142245dd90441
MaximoAndriola <maximoandriola2017@gmail.com>
2026-04-10 00:47:09 -0300

    fix: set default IVA condition to RESPONSABLE_INSCRIPTO for existing records

--- src/main/resources/db/migration/V52__add_iva_condition_to_suppliers_and_tenants.sql
+++ src/main/resources/db/migration/V52__add_iva_condition_to_suppliers_and_tenants.sql
@@ -1,2 +1,5 @@
 ALTER TABLE suppliers ADD COLUMN iva_condition VARCHAR(30);
 ALTER TABLE tenants ADD COLUMN iva_condition VARCHAR(30);
+
+UPDATE suppliers SET iva_condition = 'RESPONSABLE_INSCRIPTO' WHERE iva_condition IS NULL;
+UPDATE tenants SET iva_condition = 'RESPONSABLE_INSCRIPTO' WHERE iva_condition IS NULL;
